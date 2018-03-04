0fb49221d0b37bdf00470de1fc993862aa56f5ea
Neilai <1131894367@qq.com>
2018-03-04 14:45:05 +0800

    add find kth largest problem

--- leetcode-88.cpp
+++ leetcode-88.cpp
@@ -31,14 +31,34 @@ public:
         for(int i=0;i<m+n;i++){
         	nums1.push_back(result[i]);
         }
-        for(int i=0;i<nums1.size();i++)
-        	cout<<nums1[i];
     }
 };
-int main(){
-	Solution s;
-	std::vector<int> nums1,nums2;
-	for(int i=1;i<10;i+=2)
-		nums1.push_back(i),nums2.push_back(i+1);
-	s.merge(nums1,nums1.size(),nums2,nums2.size());
-}
+
+// 改进  双指针这样写很不优雅，可以把两个while给去掉
+// 另外 可以不用另外开辟一个变量，可以把nums1复制一份放到后面
+
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+
+        assert(nums1.size() == m + n && nums2.size() == n);
+
+        for(int i = n + m - 1 ; i >= n ; i -- )
+            nums1[i] = nums1[i - n];
+
+        int i = n;  // pointer for nums1 [n, n+m)
+        int j = 0;  // pointer for nums2 [0, n)
+        int k = 0;  // pointer merged nums1 [0, n+m)
+        while( k < n + m ){
+            if( i >= n+m )
+                nums1[k++] = nums2[j++];
+            else if( j >= n )
+                nums1[k++] = nums1[i++];
+            else if( nums1[i] < nums2[j] )
+                nums1[k++] = nums1[i++];
+            else
+                nums1[k++] = nums2[j++];
+        }
+    }
+};
