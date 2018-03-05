15cdd96d45a31a588cfa238a7fb2e3fb195fb646
Neilai <1131894367@qq.com>
2018-03-05 14:48:02 +0800

    “add leetcode-76

--- leetcode-3.cpp
+++ leetcode-3.cpp
@@ -3,6 +3,10 @@
 #include<cstdlib>
 #include<iostream>
 // 滑动窗口的经典应用
+// 这里用到了一个小技巧 字母和数字等ascci字符都在256以内，所以可以直接用一个长度为256的int数组作为hash
+// 什么时候可以用滑动窗口？
+// 连续字串相关的最优化问题可以考虑  它和双指针相似 通过一个判定变量可以排除很多数据的时候可以考虑
+// 这道题的判定变量是长度
 using namespace std;
 class Solution {
 public:
