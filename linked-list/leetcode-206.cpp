5224c2d5e1572a9862afa3859e3e5e0aba569018
Neilai <1131894367@qq.com>
2018-03-06 10:07:50 +0800

    add remove duplicates list problem

--- linked-list/leetcode-206.cpp
+++ linked-list/leetcode-206.cpp
@@ -3,33 +3,39 @@ using namespace std;
 struct ListNode {
        int val;
        ListNode *next;
-       ListNode(int x) : val(x), next(nullptr) {}
+       ListNode(int x) : val(x) {}
 };
- 
+ // 妈耶 链表反转也能把我搞出bug
+// 问题出在循环  要搞清楚循环变量的意义  也就是到达while点的时候，变量的意义 ，cur意味着当前需要改变指向的链表
+// 另外链表题一定要注意一下空的情况
+// 还有吐槽一下也没说下链表有没有头指针啥的 = =
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
+        if(!head)
+        	return head;
         ListNode *pre=nullptr;
-        ListNode *cur=head->next;
+        ListNode *cur=head;
         ListNode *tmp;
-        while(cur->next){
+        while(cur){
         	tmp=cur->next;
         	cur->next=pre;
         	pre=cur;
         	cur=tmp;
         }
-        head->next=cur;
-        return head;
+        return pre;
     }
 };
-
-
 int main(){
+
 	ListNode x(1);
 	ListNode y(2);
 	ListNode z(3);
 	ListNode *head;
-	cout<<&x;
+	head->next=head;
+	head->next=&x;
+	// cout<<x.val;
+	cout<<head->next->val;
 	// x.next=&y;
 	// y.next=&z;
 	// Solution s;
