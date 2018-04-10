987be1344013131ddea7886f15645b2d313dbd9a
Neilai <1131894367@qq.com>
2018-04-11 00:04:33 +0800

    add search folder

--- dp/leetcode-376.cpp
+++ dp/leetcode-376.cpp
@@ -16,9 +16,9 @@ public:
     	for(int i=1;i<n;i++)
     		for(int j=0;j<i;j++){
     		    if(nums[i]>nums[j])
-    					up[i]=max(up[i],up[j]+1);
-    			else
-    					down[i]=max(down[i],down[j]+1);
+    					up[i]=max(up[i],down[j]+1);
+    			else if(nums[i]<nums[j])
+    					down[i]=max(down[i],up[j]+1);
     		}
     	int res1=up[0];
     	for(int i=1;i<n;i++)
---
+++ search/leetcode-93.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cassert>
+#include <cstdlib>
+using namespace std;
+class Solution {
+public:
+	vector<string> res;
+	bool isValid(string s){
+		if(!s.size())
+			return 0;
+		else if(s.size()==1&&s[0]=='0')
+			return 1;
+		else if(s[0]=='0')
+			return 0;
+		else
+			return atoi(s.c_str())>=1&&atoi(s.c_str())<=255;
+	}
+	void addRes(vector<string> s){
+		string ip;
+		ip=s[0]+"."+s[1]+"."+s[2]+"."+s[3];
+		res.push_back(ip);
+	}
+	void findIp(string rest,vector<string> prev){
+		if(prev.size()==3){
+			if(isValid(rest)){
+				prev.push_back(rest);
+				addRes(prev);
+			}
+		}
+		else{
+			int pos=min(int(rest.size()),3);
+			for(int i=0;i<pos;i++){
+				if(isValid(rest.substr(0,i+1))){
+					vector<string> cur=prev;
+					cur.push_back(rest.substr(0,i+1));
+					findIp(rest.substr(i+1),cur);
+				}
+			}
+		}
+	}
+    vector<string> restoreIpAddresses(string s) {
+    	vector<string> prev;
+    	findIp(s,prev);
+    	for(int i=0;i<res.size();i++)
+    		cout<<res[i]<<endl;
+    	return res;
+    }
+};
+int main(){
+	// string s="123";
+ 	// char* x[3];
+	// s.copy(x[0],3,1);
+	// cout<<x[0];
+	// cout<<stoi(s.substr(1));
+	Solution s;
+	string x="010010";
+	s.restoreIpAddresses(x);
+}
