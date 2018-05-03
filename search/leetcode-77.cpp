09dd27765100a1eea5b09d4f1a9312e47543f0be
Neilai <1131894367@qq.com>
2018-05-03 11:35:32 +0800

    add combination

---
+++ search/leetcode-77.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:	
+	// vector<bool> used;
+	vector<vector<int>> res;
+	void findcombine(int n,int index,int k,vector<int> &prev){
+		if(prev.size()==k){
+			res.push_back(prev);
+			return;
+		}
+		if(n-index+1<k-prev.size())
+			return;
+		for(int i=index;i<=n;i++){
+			prev.push_back(i);
+			findcombine(n , i+1 , k , prev);
+			prev.pop_back();
+		}
+		return;
+	}
+
+    vector<vector<int>> combine(int n, int k) {
+    	res.clear();
+        if( n <= 0 || k <= 0 || k > n )
+            return res;
+    	vector<int> prev;
+    	// used = vector<bool>(n, false);
+    	findcombine(n,1,k,prev);	
+    	
+    	for(int i=0;i<res.size();i++){
+    		for(int j=0;j<res[i].size();j++)
+    			cout<<res[i][j];
+    		cout<<endl;
+    	}
+
+    	return res;
+    }
+};
+int main(){
+	Solution s;
+	s.combine(4,2);
+
+}
