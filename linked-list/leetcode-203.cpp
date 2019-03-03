060c903ab0090676e1791a88d09022762cb8276d
Neilai <1131894367@qq.com>
2019-03-03 15:07:46 +0800

    add leetcode-203

---
+++ linked-list/leetcode-203.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode *dummy=new ListNode(0),*p=dummy;
+        dummy->next=head;
+        while(p->next){
+        	if(p->next->val==val){
+        		p->next=p->next->next;
+        	}else{
+        		p=p->next;
+        	}
+        }
+        return dummy->next;
+    }
+};
+
+int main(){
+	return 1;
+}
